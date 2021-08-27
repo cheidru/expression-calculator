12d68e3d1556aa69625e75eff219e6309e6dcc5e
Igor Checheurov <chei@yandex.ru>
2021-08-27 22:35:39 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ function expressionCalculator(expr) {
     strArr = exprStr.split(''); // split string into letter array
     let inBracketsResult = {}; // first: calculation result or null if division by zero , second: index position of end
 
+    return exprStr;
+
     // check brackets pairing
     if (checkBrackets(strArr) === false) throw new ExpressionError("ExpressionError: Brackets must be paired");
 
     let stack = [];
 
-    return srtArr;
+
 
     for (let i = 0; i < strArr.length; i++) {
 
